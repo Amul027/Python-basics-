3bb9e458e30e62c2d5f2a29e1e586d2bbd2d6b79
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:18:13 +0530

    Add files via upload

---
+++ list methods.py
@@ -0,0 +1,37 @@
+my_list=[1,2,3,4]
+'''
+list methods:
+1.sort()
+2.clear()
+3.append()
+4.count()
+5.extend([4])
+6.insert()
+7.index()
+8.len()
+9.remove()
+10.pop()
+11.reverse()
+12.copy()
+13.len()
+14.min()
+15.max()
+16.del()
+'''
+mylist ={1,2,3,4,5,6}
+mylist.sort()
+print(mylist)
+mylist.append(99)
+print(mylist)
+mylist.count(4)
+print(mylist.count(4))
+mylist.extend([44,77,88])
+print(mylist)
+max =max(mylist)
+print(max)
+min=min(mylist)
+print(min)
+del mylist[2]
+print(mylist)
+copylist=mylist.copy()
+print("copied list="copylist)
