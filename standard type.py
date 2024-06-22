95e99dd558144f081b67038bafd08ec1f0f0d393
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:20:54 +0530

    Add files via upload

---
+++ standard type.py
@@ -0,0 +1,37 @@
+# Integer
+x = 5
+
+# Float
+y = 3.14
+
+# String
+message = "Hello, world!"
+
+# Boolean
+is_valid = True
+
+# List
+my_list = [1, 2, 3, 'a', 'b', 'c']
+
+# Tuple
+my_tuple = (1, 2, 3, 'a', 'b', 'c')
+
+# Dictionary
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+
+# Set
+my_set = {1, 2, 3, 4, 5}
+
+# NoneType
+result = None
+
+# Printing the values
+print("Integer:", x)
+print("Float:", y)
+print("String:", message)
+print("Boolean:", is_valid)
+print("List:", my_list)
+print("Tuple:", my_tuple)
+print("Dictionary:", my_dict)
+print("Set:", my_set)
+print("NoneType:", result)
