ec46267c6e4c869d86c3b419531a8ba9591d43f2
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:11:40 +0530

    Add files via upload

---
+++ pickledemo.py
@@ -0,0 +1,11 @@
+'''import pickle
+
+fp = open("picklefile.txt", "wb")
+
+names = ["dhoni", "virat", "dhawan"]
+
+pickle.dump(names, fp)
+fp.close()'''
+import picklefp=open("picklefile.txt","vb")
+unpickle=pickle.load(fp):
+print(unpickle)
