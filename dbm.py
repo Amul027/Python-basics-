9736093f72bf79384474e5812edcfbe8ceb51112
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:16:30 +0530

    Add files via upload

---
+++ dbm.py
@@ -0,0 +1,11 @@
+'''dbm.open
+db=dbm.open("dbm1.db",'r')
+
+for k,v in db.items()
+     print(k,v)'''
+class myclass:
+  def  __int__(self,name,rollno)
+    sef.name=name
+    self.rollno=rollno
+    p1=myclass("hello","hi")
+    print(p1.name)
