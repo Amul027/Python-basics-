b1986798a1adc3ffb17861971df4ca4b429d3525
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:21:08 +0530

    Add files via upload

---
+++ w4.3.py
@@ -0,0 +1,13 @@
+n=int(input("Enter no of values"))
+dic={}
+for i in range(n):
+    key=input("Enter key")
+    value=input("Enter value")
+    dic[key]=value
+    print(dic)
+    print("after invertion")
+    print(invert_content(dic))
+    def invent_content(dic):ic.items()}
+        return invertdk
+        invert_dic={}
+        invert_dic={v:k for k,v ,d
