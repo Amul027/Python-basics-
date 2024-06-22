2f44a1ab4f77899c66e557955e1d56e67275892d
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:17:22 +0530

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,7 @@
+
+     a=int(input("enter the a values"))
+     b=int(input("enter the a values"))
+     if b==0:
+         raise print("Arthematic exception")
+     else:
+          print("division=",a/b)
