014c8208059e23414f666fa850f8221f1c363b2e
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:19:42 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+port=9000
+Handler=http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("",port),Handler) as httpd:
+    print("Serving at port ",port)
+    print("Amul")
+    httpd.serve_forever()
+    
+
