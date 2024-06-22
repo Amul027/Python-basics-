73dda4b778e36a074bc1f99c5fef404dc64dc967
Amul027 <161427279+Amul027@users.noreply.github.com>
2024-06-22 12:13:17 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+def remove_duplicates(li):
+    unique_list=[]=1,2,3,5,7
+        duplicates_list=[],2,5
+    for in range(li):
+        if i in unique_list
+    unique list.append(i)
+    else:
+         duplicate_list.append(i)
+    return unique_list
+    li=[1,2,3,4,5,6]
+    print(li)
+    print(remove_duplicates(li))
+
